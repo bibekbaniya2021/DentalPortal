1666db7f28b10ec348869b1b3310f5dbd56662cb
codebibek <97448890+bibekbaniya2021@users.noreply.github.com>
2022-04-01 11:26:22 +0000

    Added home page, edited css etc

--- dental/settings.py
+++ dental/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'django.contrib.admin',    
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+  
+    'website',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
